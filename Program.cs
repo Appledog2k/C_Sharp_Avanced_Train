c6c539eaa868218dd7d521d2aba4d523110f431c
Appledog2k <Appledog2k@gmail.com>
2022-03-16 16:10:28 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int a=0,b=1,c=2;
+int result = a > b ? a > c ? a : c :  b > c ? b :c ;
+System.Console.WriteLine(result);
+
